0a4d233eafa95c548da405bf1ef94d1321a4f9ce
Nat <sqlnat@gmail.com>
2018-04-28 19:27:37 +0100

    Added deployment script

---
+++ CorporateDataWarehouse/CorporateDataWarehouse/Deploy/Deploy.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\SQlPackage.exe" /Action:Publish /SourceFile:C:\Program Files (x86)\Jenkins\workspace\CorporateDataWarehouse_Build\CorporateDataWarehouse\CorporateDataWarehouse\bin\Debug\CorporateDataWarehouse.dacpac /TargetDatabaseName:EDW_DEV /TargetServerName:"localhost"
+
+
